aded77792c561771a5cd80a7730bfa4aae35f911
Samuel Jacobs <108878134+SamuelJacobsHMRC@users.noreply.github.com>
2023-01-10 14:16:16 +0000

    MTDSA-14345: ISpec and TaxYearNotEndedError workaround

--- app/config/FeatureSwitches.scala
+++ app/config/FeatureSwitches.scala
@@ -16,7 +16,9 @@
 
 package config
 
+import org.apache.commons.lang3.BooleanUtils
 import play.api.Configuration
+import play.api.mvc.Request
 
 case class FeatureSwitches(featureSwitchConfig: Configuration) {
 
@@ -39,6 +41,14 @@ case class FeatureSwitches(featureSwitchConfig: Configuration) {
 
   val isTaxYearNotEndedRuleEnabled: Boolean = isEnabled("taxYearNotEndedRule.enabled")
   val isTaxYearSpecificApiEnabled: Boolean  = isEnabled("tys-api.enabled")
+
+  def isTemporalValidationEnabled(implicit request: Request[_]): Boolean = {
+    if (isEnabled("allowTemporalValidationSuspension.enabled")) {
+      request.headers.get("suspend-temporal-validations").forall(!BooleanUtils.toBoolean(_))
+    } else {
+      true
+    }
+  }
 
   private def isEnabled(key: String): Boolean = featureSwitchConfig.getOptional[Boolean](key).getOrElse(true)
 }
--- app/v1/controllers/UnignoreBenefitController.scala
+++ app/v1/controllers/UnignoreBenefitController.scala
@@ -18,7 +18,7 @@ package v1.controllers
 
 import cats.data.EitherT
 import cats.implicits._
-import config.AppConfig
+import config.{AppConfig, FeatureSwitches}
 import play.api.libs.json.Json
 import play.api.mvc.{Action, AnyContent, ControllerComponents}
 import play.mvc.Http.MimeTypes
@@ -63,7 +63,8 @@ class UnignoreBenefitController @Inject() (val authService: EnrolmentsAuthServic
       val rawData: IgnoreBenefitRawData = IgnoreBenefitRawData(
         nino = nino,
         taxYear = taxYear,
-        benefitId = benefitId
+        benefitId = benefitId,
+        temporalValidationEnabled = FeatureSwitches()(appConfig).isTemporalValidationEnabled
       )
 
       val result =
