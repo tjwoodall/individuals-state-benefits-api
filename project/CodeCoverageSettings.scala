21f4d6d3b9895e5a1ca783653703a80748af827b
jeremystone <6453521+jeremystone@users.noreply.github.com>
2024-09-24 09:25:49 +0100

    - revert code coverage settings

--- project/CodeCoverageSettings.scala
+++ project/CodeCoverageSettings.scala
@@ -33,7 +33,7 @@ object CodeCoverageSettings {
 
   val settings: Seq[Setting[_]] = Seq(
     ScoverageKeys.coverageExcludedPackages := excludedPackages.mkString(";"),
-    ScoverageKeys.coverageMinimumStmtTotal := 94,
+    ScoverageKeys.coverageMinimumStmtTotal := 95,
     ScoverageKeys.coverageFailOnMinimum := true,
     ScoverageKeys.coverageHighlighting := true
   )
